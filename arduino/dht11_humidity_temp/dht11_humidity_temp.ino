beeed4870f0c897391aa0bc09de268ec7f0b1524
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2017-06-04 18:19:00 +0000

    trunk/ (saulius@kolibris.saulius-grazulis.lt) Attaching the second, DHT22, sensor, and displaying information from both sensors.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@20 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/dht11_humidity_temp/dht11_humidity_temp.ino
+++ arduino/dht11_humidity_temp/dht11_humidity_temp.ino
@@ -3,9 +3,11 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-dht DHT;
+dht DHT11;
+dht DHT22;
 
 #define DHT11_PIN 7
+#define DHT22_PIN 8
 
 int step = 0;
 int ledPinNo = 13;
@@ -18,16 +20,35 @@ void setup()
 
 void loop()
 {
-  int chk = DHT.read11(DHT11_PIN);
+  int chk_dht11 = DHT11.read11(DHT11_PIN);
+  int chk_dht22 = DHT22.read(DHT22_PIN);
+
   lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
-  lcd.print(DHT.temperature);
-  lcd.print((char)223);
-  lcd.print("C");
+  lcd.print("DTH11: ");
+  if( chk_dht11 == DHTLIB_OK ) {
+    lcd.print(DHT11.temperature, 0);
+    lcd.print((char)223);
+    lcd.print("C ");
+    lcd.print(DHT11.humidity, 0);
+    lcd.print("% ");
+  } else {
+    lcd.print("T=?? ");
+    lcd.print("RH=?");    
+  }
+
   lcd.setCursor(0,1);
-  lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);
-  lcd.print("%");
+  lcd.print("DHT22: ");
+  if( chk_dht22 == DHTLIB_OK ) {
+    lcd.print(DHT22.temperature, 0);
+    lcd.print((char)223);
+    lcd.print("C ");
+    lcd.print(DHT22.humidity, 0);
+    lcd.print("% ");
+  } else {
+    lcd.print("T=?? ");
+    lcd.print("RH=?");    
+  }
+
   if( step == 0 ) {
     digitalWrite( ledPinNo, HIGH );
     step = 1;
@@ -35,5 +56,6 @@ void loop()
     digitalWrite( ledPinNo, LOW );
     step = 0;
   }
+
   delay(1000);
 }
