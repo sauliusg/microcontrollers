3b12e5f1bc508d5ddacc8565ddb1aba39b1f9045
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2017-06-15 07:30:20 +0000

    trunk/arduino/ (saulius@koala.ibt.lt) Adding provisions to attache the second DHT11 sensor. Icreasing read-out delay to 2s (from 1s).
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@25 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/dht11_humidity_temp/dht11_humidity_temp.ino
+++ arduino/dht11_humidity_temp/dht11_humidity_temp.ino
@@ -3,10 +3,12 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-dht DHT11;
+dht DHT11_1;
+dht DHT11_2;
 dht DHT22;
 
-#define DHT11_PIN 7
+#define DHT11_1_PIN 6
+#define DHT11_2_PIN 7
 #define DHT22_PIN 8
 
 int step = 0;
@@ -24,20 +26,37 @@ int chk_dht22;
 
 void loop()
 {
-  chk_dht11 = step == 0 ? DHT11.read11(DHT11_PIN) : chk_dht11;
+  chk_dht11 = step == 0 ? DHT11_1.read11(DHT11_1_PIN) : DHT11_2.read11(DHT11_2_PIN);
   chk_dht22 = DHT22.read(DHT22_PIN);
 
   lcd.setCursor(0,0); 
-  lcd.print("DHT11: ");
+  lcd.print("11(");
+  lcd.print(step+1);
+  lcd.print("): ");
+
+  Serial.print("11(");
+  Serial.print(step+1);
+  Serial.print("): ");
+
   if( chk_dht11 == DHTLIB_OK ) {
-    lcd.print(DHT11.temperature, 0);
+    double temperature = step == 0 ? DHT11_1.temperature : DHT11_2.temperature;
+    double humidity = step == 0 ? DHT11_1.humidity : DHT11_2.humidity;
+    lcd.print(temperature, 0);
     lcd.print((char)223);
     lcd.print("C ");
-    lcd.print(DHT11.humidity, 0);
+    lcd.print(humidity, 0);
     lcd.print("% ");
+
+    Serial.print("T = ");
+    Serial.print(temperature);
+    Serial.print(" RH = ");
+    Serial.print(humidity);
+    Serial.print(" ");
   } else {
     lcd.print("T=?? ");
     lcd.print("RH=?");    
+    Serial.print("T = ?? ");
+    Serial.print("RH = ??");    
   }
 
   lcd.setCursor(0,1);
@@ -53,16 +72,18 @@ void loop()
     lcd.print("RH=?");    
   }
 
-  Serial.print("DTH11: T = ");
-  Serial.print(DHT11.temperature);
-  Serial.print(" RH = ");
-  Serial.print(DHT11.humidity);
-  Serial.print(" ");
-
   Serial.print("DTH22: T = ");
-  Serial.print(DHT22.temperature);
+  if( chk_dht22 == DHTLIB_OK ) {
+    Serial.print(DHT22.temperature);
+  } else {
+    Serial.print( "??" );
+  }
   Serial.print(" RH = ");
-  Serial.print(DHT22.humidity);
+  if( chk_dht22 == DHTLIB_OK ) {
+    Serial.print(DHT22.humidity);
+  } else {
+    Serial.print( "??" );
+  }  
   Serial.println("");
 
   if( step == 0 ) {
@@ -73,5 +94,5 @@ void loop()
     step = 0;
   }
 
-  delay(1000);
+  delay(2000);
 }
