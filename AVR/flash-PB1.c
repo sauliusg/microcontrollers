73e9ca6c51693dc4ea30996bc642e7610a2d499f
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2018-01-13 21:30:07 +0000

    trunk/ (saulius@drugelis.saulius-grazulis.lt) Adding a simple test to flash Atmega8 on PB1 (pin 15), sucessfully uploaded via ArduinoISP.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@63 931b4fa1-5130-4491-9317-3c5e5f8f39d3

---
+++ AVR/flash-PB1.c
@@ -0,0 +1,47 @@
+#include <avr/io.h>
+
+#define sbi(REGISTER,BIT) REGISTER |= (1 << BIT);    /* sets BIT in REGISTER */
+#define cbi(REGISTER,BIT) REGISTER &= ~(1 << BIT);   /* clears BIT in REGISTER */
+
+void delay_ms(unsigned short ms)
+/* delay for a minimum of <ms> */
+/* with a 4Mhz crystal, the resolution is 1 ms */
+{
+    unsigned short inner;
+
+    while ( ms ) {
+	ms--;
+	inner = 200;
+	while (inner) {
+	    inner--;
+	}
+    }
+}
+
+void flash(short flash, short pause)
+{
+    /* LED on */
+    sbi(PORTB,PB1);
+    delay_ms(flash);
+    /* LED off */
+    cbi(PORTB,PB1);
+    delay_ms(pause);
+}
+
+int main(void)
+{
+    short i;
+        /* enable selected pin as an output: */
+        sbi(DDRB,PB1);
+        // DDRB=2;
+        while (1) {
+	    flash(200,200);
+	    flash(200,200);
+	    flash(200,200);
+	    delay_ms(1500);
+	    for( i = 100; i >= 20; i -= 10 ) {
+		flash(i,i);
+	    }
+        }
+        return 0;
+}
