dd4854ce51a19690725a035d7bdcdf5b0f798679
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2018-02-18 20:48:03 +0000

    trunk/arduino/ (saulius@drugelis.saulius-grazulis.lt) Reading a sector.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@129 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/test_rfid_RC522/test_rfid_RC522.ino
+++ arduino/test_rfid_RC522/test_rfid_RC522.ino
@@ -36,6 +36,8 @@
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance.
 
+MFRC522::MIFARE_Key key;
+
 /**
  * Helper routine to dump a byte array as hex values to Serial.
  */
@@ -62,20 +64,43 @@ void loop() {
 	// Dump debug info about the card. PICC_HaltA() is automatically called.
 	// mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
+        // Prepare the key (used both as key A and as key B)
+        // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
+        for (byte i = 0; i < 6; i++) {
+            key.keyByte[i] = 0xFF;
+        }
+        
+        MFRC522::StatusCode status;
+        byte sector = 1;
+        byte trailerBlock = 7;
+        // Authenticate using key A
+        Serial.println(F("Authenticating using key A..."));
+        status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,
+                                                                trailerBlock, &key, &(mfrc522.uid));
+        if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("PCD_Authenticate() failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            return;
+        }
+        
+        // Dump a sector:
+        mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, sector);
+        Serial.println();
+        
         // Example from
         // https://github.com/miguelbalboa/rfid/blob/master/examples/ReadAndWrite/ReadAndWrite.ino#L130/
         // Read a block:
-        byte buffer[18];
-        byte size = sizeof(buffer);
-        byte blockAddr = 4;
-        MFRC522::StatusCode status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(blockAddr, buffer, &size);
-        if (status != MFRC522::STATUS_OK) {
-            Serial.print(F("MIFARE_Read() failed: "));
-            Serial.println(mfrc522.GetStatusCodeName(status));
-        }
-        Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
-        dump_byte_array(buffer, 16); Serial.println();
-        mfrc522.PICC_HaltA();
+        ///byte buffer[18];
+        ///byte size = sizeof(buffer);
+        ///byte blockAddr = 4;
+        ///MFRC522::StatusCode status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(blockAddr, buffer, &size);
+        ///if (status != MFRC522::STATUS_OK) {
+        ///    Serial.print(F("MIFARE_Read() failed: "));
+        ///    Serial.println(mfrc522.GetStatusCodeName(status));
+        ///}
+        ///Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
+        ///dump_byte_array(buffer, 16); Serial.println();
+        ///mfrc522.PICC_HaltA();
 }
 
 /**
