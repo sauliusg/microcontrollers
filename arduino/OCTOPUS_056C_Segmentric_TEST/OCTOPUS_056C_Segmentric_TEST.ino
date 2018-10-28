5d4ba65888c5b2fb7b893eb4ce7e51fd5f1d2748
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2018-10-28 07:30:24 +0000

    trunk/ (saulius@drugelis.saulius-grazulis.lt) Adding OCTOPUS 7-seg indicator example (uses Adafruit libraries).
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@217 931b4fa1-5130-4491-9317-3c5e5f8f39d3

---
+++ arduino/OCTOPUS_056C_Segmentric_TEST/OCTOPUS_056C_Segmentric_TEST.ino
@@ -0,0 +1,31 @@
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+//#include <TinyWireM.h> // Enable this line if using Adafruit Trinket, Gemma, etc.
+
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+void setup() {
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
+}
+
+void loop() {
+
+  matrix.writeDigitNum(0, 8, true);
+  matrix.writeDigitNum(1, 8, true);
+  matrix.drawColon(true);
+  matrix.writeDigitNum(3, 8, true);
+  matrix.writeDigitNum(4, 8, true);
+  matrix.writeDisplay();
+  delay(500);
+  matrix.clear();
+  matrix.writeDisplay();
+  delay(500);
+ 
+}
+
