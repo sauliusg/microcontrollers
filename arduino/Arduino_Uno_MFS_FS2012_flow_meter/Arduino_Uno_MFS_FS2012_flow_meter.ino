61fc0097f4f89cfb71c90cd13ea5a5abddb7af51
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2019-01-12 18:44:17 +0000

    trunk/ (saulius@varanas.saulius-grazulis.lt) Playing with segments, adding counter.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@241 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/Arduino_Uno_MFS_FS2012_flow_meter/Arduino_Uno_MFS_FS2012_flow_meter.ino
+++ arduino/Arduino_Uno_MFS_FS2012_flow_meter/Arduino_Uno_MFS_FS2012_flow_meter.ino
@@ -7,7 +7,40 @@ const byte LED[] = {13,12,11,10};
 #define LATCH_DIO 4
 #define CLK_DIO 7
 #define DATA_DIO 8
- 
+
+/*
+ Segment layout -- segments are assigned to the shifter
+ parallel output pins:
+*/
+
+#define SEG_A 0x80
+#define SEG_B 0x40
+#define SEG_C 0x20
+#define SEG_D 0x10
+#define SEG_E 0x08
+#define SEG_F 0x04
+#define SEG_G 0x02
+#define SEG_H 0x01
+
+static short digit7seg[] = {
+    /* 0 */ 0xFF & (~ (SEG_G | SEG_H )),
+    /* 1 */ SEG_B + SEG_C,
+    /* 2 */ SEG_A + SEG_B + SEG_G + SEG_E + SEG_D,
+    /* 3 */ SEG_A + SEG_B + SEG_G + SEG_C + SEG_D,
+    /* 4 */ SEG_F + SEG_G + SEG_B + SEG_C,
+    /* 5 */ SEG_A + SEG_F + SEG_G + SEG_C + SEG_D,
+    /* 6 */ 0xFF & (~ (SEG_B | SEG_H)),
+    /* 7 */ SEG_A + SEG_B + SEG_C,
+    /* 8 */ 0xFF & (~SEG_H),
+    /* 6 */ 0xFF & (~ (SEG_E | SEG_H)),
+    /* A */ 0xFF & (~ (SEG_D | SEG_H)),
+    /* B */ SEG_F + SEG_G + SEG_E + SEG_C + SEG_D,
+    /* C */ SEG_A + SEG_F + SEG_E + SEG_D,
+    /* D */ SEG_B + SEG_G + SEG_E + SEG_C + SEG_D,
+    /* E */ SEG_A + SEG_F + SEG_G + SEG_E + SEG_D,
+    /* F */ SEG_A + SEG_F + SEG_G + SEG_E,
+};
+
 /* Segment byte maps for numbers 0 to 9 */
 const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
 /* Byte maps to select digit 1 to 4 */
@@ -34,13 +67,13 @@ int counter = 0;
 
 void loop()
 {
-  //counter ++;
   /* Update the display with the current counter value */
-  WriteNumberToSegment( 0, 1 );
-  WriteNumberToSegment( 1, 2 );
-  WriteNumberToSegment( 2, 3 );
-  WriteNumberToSegment( 3, 4 );
-  //delay(2000);
+  //WriteNumberToSegment( 0, 1 );
+  //WriteNumberToSegment( 1, 2 );
+  //WriteNumberToSegment( 2, 3 );
+  WriteNumberToSegment( 3, counter%10 );
+  delay(1000);
+  counter ++;
 }
 
 /* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
