4c21f3a391cd279431b995467d4e7eb0738f257a
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2019-01-14 11:40:30 +0000

    trunk/ (saulius@koala.ibt.lt) Preparing Ardiono sketch for the FS2012 flow meter with the 74HC4094 based dual (2x4 digits) multiplexed 7seg indicator.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@244 931b4fa1-5130-4491-9317-3c5e5f8f39d3

---
+++ arduino/Arduino_Uno_74HC4094_FS2012_flow_meter/Arduino_Uno_74HC4094_FS2012_flow_meter.ino
@@ -0,0 +1,86 @@
+const byte LED[] = {13,12,11,10};
+ 
+#define BUTTON1 A1
+#define BUTTON2 A2
+
+/* Define shift register pins used for seven segment display */
+#define LATCH_DIO 4
+#define CLK_DIO 7
+#define DATA_DIO 8
+
+/*
+ Segment layout -- segments are assigned to the shifter
+ parallel output pins:
+*/
+
+#define SEG_A 0x80
+#define SEG_B 0x40
+#define SEG_C 0x20
+#define SEG_D 0x10
+#define SEG_E 0x08
+#define SEG_F 0x04
+#define SEG_G 0x02
+#define SEG_H 0x01
+
+static short digit7seg[] = {
+    /* 0 */ 0xFF & (~ (SEG_G | SEG_H )),
+    /* 1 */ SEG_B + SEG_C,
+    /* 2 */ SEG_A + SEG_B + SEG_G + SEG_E + SEG_D,
+    /* 3 */ SEG_A + SEG_B + SEG_G + SEG_C + SEG_D,
+    /* 4 */ SEG_F + SEG_G + SEG_B + SEG_C,
+    /* 5 */ SEG_A + SEG_F + SEG_G + SEG_C + SEG_D,
+    /* 6 */ 0xFF & (~ (SEG_B | SEG_H)),
+    /* 7 */ SEG_A + SEG_B + SEG_C,
+    /* 8 */ 0xFF & (~SEG_H),
+    /* 6 */ 0xFF & (~ (SEG_E | SEG_H)),
+    /* A */ 0xFF & (~ (SEG_D | SEG_H)),
+    /* B */ SEG_F + SEG_G + SEG_E + SEG_C + SEG_D,
+    /* C */ SEG_A + SEG_F + SEG_E + SEG_D,
+    /* D */ SEG_B + SEG_G + SEG_E + SEG_C + SEG_D,
+    /* E */ SEG_A + SEG_F + SEG_G + SEG_E + SEG_D,
+    /* F */ SEG_A + SEG_F + SEG_G + SEG_E,
+};
+
+/* Segment byte maps for numbers 0 to 9 */
+const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
+/* Byte maps to select digit 1 to 4 */
+const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};
+
+void WriteNumberToSegment(byte Segment, byte Value);
+
+void setup()
+{
+  // initialize the digital pin as an output.
+  /* Set each pin to outputs */
+  pinMode(LED[0], OUTPUT);
+  pinMode(LED[1], OUTPUT);
+  pinMode(LED[2], OUTPUT);
+  pinMode(LED[3], OUTPUT);
+  /* Set DIO pins to outputs */
+  pinMode(LATCH_DIO,OUTPUT);
+  pinMode(CLK_DIO,OUTPUT);
+  pinMode(DATA_DIO,OUTPUT);
+
+}
+
+int counter = 0;
+
+void loop()
+{
+  /* Update the display with the current counter value */
+  //WriteNumberToSegment( 0, 1 );
+  //WriteNumberToSegment( 1, 2 );
+  //WriteNumberToSegment( 2, 3 );
+  WriteNumberToSegment( 3, 5 );
+  delay(1000);
+  counter ++;
+}
+
+/* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
+void WriteNumberToSegment(byte Segment, byte Value)
+{
+  digitalWrite(LATCH_DIO,LOW);
+  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]);
+  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] );
+  digitalWrite(LATCH_DIO,HIGH);
+}
