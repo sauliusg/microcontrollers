d6d666eefad1b8d9de0ad62a3a0e62ec45699941
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2019-01-14 11:50:03 +0000

    trunk/ (saulius@koala.ibt.lt) Adding the working 7-seg indicator checker.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@245 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/Arduino_Uno_74HC4094_FS2012_flow_meter/Arduino_Uno_74HC4094_FS2012_flow_meter.ino
+++ arduino/Arduino_Uno_74HC4094_FS2012_flow_meter/Arduino_Uno_74HC4094_FS2012_flow_meter.ino
@@ -1,12 +1,8 @@
-const byte LED[] = {13,12,11,10};
- 
-#define BUTTON1 A1
-#define BUTTON2 A2
-
 /* Define shift register pins used for seven segment display */
-#define LATCH_DIO 4
-#define CLK_DIO 7
-#define DATA_DIO 8
+#define LATCH_DIO 10
+#define CLK_DIO   14
+#define DATA_DIO  16
+#define OE         9
 
 /*
  Segment layout -- segments are assigned to the shifter
@@ -44,43 +40,38 @@ static short digit7seg[] = {
 /* Segment byte maps for numbers 0 to 9 */
 const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
 /* Byte maps to select digit 1 to 4 */
-const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};
+const byte SEGMENT_SELECT[] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 
 void WriteNumberToSegment(byte Segment, byte Value);
 
 void setup()
 {
   // initialize the digital pin as an output.
-  /* Set each pin to outputs */
-  pinMode(LED[0], OUTPUT);
-  pinMode(LED[1], OUTPUT);
-  pinMode(LED[2], OUTPUT);
-  pinMode(LED[3], OUTPUT);
   /* Set DIO pins to outputs */
   pinMode(LATCH_DIO,OUTPUT);
   pinMode(CLK_DIO,OUTPUT);
   pinMode(DATA_DIO,OUTPUT);
-
+  pinMode(OE,OUTPUT);
 }
 
 int counter = 0;
 
 void loop()
 {
+  digitalWrite(OE,HIGH);
   /* Update the display with the current counter value */
-  //WriteNumberToSegment( 0, 1 );
-  //WriteNumberToSegment( 1, 2 );
-  //WriteNumberToSegment( 2, 3 );
-  WriteNumberToSegment( 3, 5 );
-  delay(1000);
+  for( int i = 0; i < 8; i++ ) {
+    WriteNumberToSegment( i, counter%10 );
+    delay(200);
+  }
   counter ++;
 }
 
 /* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
 void WriteNumberToSegment(byte Segment, byte Value)
 {
-  digitalWrite(LATCH_DIO,LOW);
+  digitalWrite(LATCH_DIO,HIGH);
   shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]);
   shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] );
-  digitalWrite(LATCH_DIO,HIGH);
+  digitalWrite(LATCH_DIO,LOW);
 }
