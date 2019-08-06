baae28e838a82c416ebb437755587b7ac4ad9574
saulius <saulius@931b4fa1-5130-4491-9317-3c5e5f8f39d3>
2019-08-06 06:30:48 +0000

    trunk/arduino/ (saulius@varanas.saulius-grazulis.lt) Adding function to read buttons with debouncing, check for continuously pressed buttons, and test them using LEDs. The continous press counter is still too short.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/microcontrollers/trunk@264 931b4fa1-5130-4491-9317-3c5e5f8f39d3

--- arduino/Arduino_Uno_MFS_Test/Arduino_Uno_MFS_Test.ino
+++ arduino/Arduino_Uno_MFS_Test/Arduino_Uno_MFS_Test.ino
@@ -7,6 +7,8 @@
 #define BUTTON2 A2
 #define BUTTON3 A3
 
+const byte LED[] = {13,12,11,10};
+
 #define BUZZER 3
 
 #define ON             LOW
@@ -21,13 +23,20 @@ const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};
  
 void setup ()
 {
-  /* Set DIO pins to outputs */
+  /* Set DIO pins */
   pinMode(LATCH_DIO,OUTPUT);
   pinMode(CLK_DIO,OUTPUT);
   pinMode(DATA_DIO,OUTPUT);
+
   pinMode(BUTTON1,INPUT);
   pinMode(BUTTON2,INPUT);  
-  pinMode(BUTTON3,INPUT);  
+  pinMode(BUTTON3,INPUT);
+
+  pinMode(LED[0], OUTPUT);
+  pinMode(LED[1], OUTPUT);
+  pinMode(LED[2], OUTPUT);
+  pinMode(LED[3], OUTPUT);
+
   pinMode(BUZZER,OUTPUT);
   digitalWrite( BUZZER, OFF );
 }
@@ -40,9 +49,62 @@ int counter = 0;
 byte digit = 0;
 byte segmentMap = 1;
 
-#define DEBOUNCING_LIMIT 200
+#define DEBOUNCING_LIMIT 150
+#define PRESS_LIMIT (DEBOUNCING_LIMIT + 60000)
 
 byte debouncing_counter;
+unsigned int debouncing_counters[3];
+
+/*
+ * Determines whether a button is pressed and if pressed, for how long. 
+ * Uses delay for debouncing. Determines if a button is pressed and held
+ * (beyond the debouncing time).
+
+ * Button ports must be properly initialised for input before calling 
+ * this function.
+
+ * Return value: 
+ *   0 -- button not pressed;
+ *   
+ *   1 -- button pressed for the first time (either still within
+ *        the DEBOUNCING_LIMIT, or wa not relesed after that)
+ *        
+ *   2 -- button is pressed and held (longer than PRESS_LIMIT)
+
+*/
+byte read_button_state( int button, unsigned int *dbcounter )
+{
+  byte b = digitalRead( button );
+  byte bvalue = 0;
+  if( b == 0 ) {
+    // Button closed
+    if( *dbcounter == 0 ) {
+      // Button pressed for the first time, start debouncing:
+      (*dbcounter) ++;
+      bvalue = 1;
+    } else
+    if( *dbcounter <= DEBOUNCING_LIMIT ) {
+      (*dbcounter) ++;
+      bvalue = 1;
+    } else
+    if( *dbcounter <= PRESS_LIMIT ) {
+      if( *dbcounter < PRESS_LIMIT )
+        (*dbcounter) ++;
+      bvalue = 2;
+    }
+  } else {
+    if( *dbcounter > DEBOUNCING_LIMIT ) {
+      *dbcounter = 0;
+      bvalue = 0;
+    } else
+    if( *dbcounter > 0 ) {
+      (*dbcounter) ++;
+      bvalue = 1;
+    }
+  }
+
+  return bvalue;
+}
 
 /* Main program */
 void loop()
@@ -69,12 +131,36 @@ void loop()
     if( segmentMap > 0x20 )
       segmentMap = 1;
   }
-  byte b1 = digitalRead( BUTTON1 );
-  byte b2 = digitalRead( BUTTON2 );
-  if( b1 == 0 )
+  byte b1 = read_button_state( BUTTON1, &debouncing_counters[0] );
+  byte b2 = read_button_state( BUTTON2, &debouncing_counters[1] );
+  if( b1 != 0 )
     write_numbers = 1;
-  if( b2 == 0 )
+  if( b2 != 0 )
     write_numbers = 0;
+
+  if( b1 == 0 ) {
+    digitalWrite( LED[0], OFF );
+    digitalWrite( LED[1], OFF );
+  } else
+  if( b1 == 1 ) {
+    digitalWrite( LED[0], ON );
+  } else
+  if( b1 == 2 ) {
+    digitalWrite( LED[0], ON );
+    digitalWrite( LED[1], ON );
+  }
+  
+  if( b2 == 0 ) {
+    digitalWrite( LED[2], OFF );
+    digitalWrite( LED[3], OFF );
+  } else
+  if( b2 == 1 ) {
+    digitalWrite( LED[2], ON );
+  } else
+  if( b2 == 2 ) {
+    digitalWrite( LED[3], ON );
+  }
+  
   byte b3 = digitalRead( BUTTON3 );
   if( b3 == 0 ) {
     // Button 3 pressed:
